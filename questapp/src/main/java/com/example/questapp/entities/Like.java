5007601ecb5f7e9da3f858c858fe5995fee7fb90
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-16 23:01:05 +0300

    Like entity, post ve user entity ile manytoone maplendi

--- questapp/src/main/java/com/example/questapp/entities/Like.java
+++ questapp/src/main/java/com/example/questapp/entities/Like.java
@@ -1,10 +1,11 @@
 package com.example.questapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="p_like")
@@ -13,6 +14,15 @@ public class Like {
 
     @Id
     Long id;
-    Long postId;
-    Long userId;
+    @ManyToOne(fetch = FetchType.LAZY) //user objesini databaseden hemen çekme, post objesini çektiğimde ilgili userı bana getirmene gerek yok//Çekmesini isteseydi LAZY yerine EAGER olacaktı
+    @JoinColumn(name="post_id",nullable=false)
+    @OnDelete(action= OnDeleteAction.CASCADE)
+    @JsonIgnore //fetch de etmeyeceğiz zaten lazy, bu alanla işimiz yok diyoruz
+    Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY) //user objesini databaseden hemen çekme, post objesini çektiğimde ilgili userı bana getirmene gerek yok//Çekmesini isteseydi LAZY yerine EAGER olacaktı
+    @JoinColumn(name="user_id",nullable=false)
+    @OnDelete(action= OnDeleteAction.CASCADE)
+    @JsonIgnore //fetch de etmeyeceğiz zaten lazy, bu alanla işimiz yok diyoruz
+    User user;
 }
