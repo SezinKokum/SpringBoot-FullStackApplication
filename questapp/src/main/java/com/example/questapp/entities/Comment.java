48b3813ab3568b9e1d2ef3a508c06679c6a93805
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-16 23:00:44 +0300

    Comment entity, post ve user entity ile manytoone maplendi

--- questapp/src/main/java/com/example/questapp/entities/Comment.java
+++ questapp/src/main/java/com/example/questapp/entities/Comment.java
@@ -1,6 +1,9 @@
 package com.example.questapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -12,7 +15,18 @@ public class Comment {
     @Id
     Long id;
     Long postId;
-    Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY) //user objesini databaseden hemen çekme, post objesini çektiğimde ilgili userı bana getirmene gerek yok//Çekmesini isteseydi LAZY yerine EAGER olacaktı
+    @JoinColumn(name="post_id",nullable=false)
+    @OnDelete(action= OnDeleteAction.CASCADE)
+    @JsonIgnore //fetch de etmeyeceğiz zaten lazy, bu alanla işimiz yok diyoruz
+    Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY) //user objesini databaseden hemen çekme, post objesini çektiğimde ilgili userı bana getirmene gerek yok//Çekmesini isteseydi LAZY yerine EAGER olacaktı
+    @JoinColumn(name="user_id",nullable=false)
+    @OnDelete(action= OnDeleteAction.CASCADE)
+    @JsonIgnore //fetch de etmeyeceğiz zaten lazy, bu alanla işimiz yok diyoruz
+    User user;
 
     @Lob
     @Column(columnDefinition="text")
