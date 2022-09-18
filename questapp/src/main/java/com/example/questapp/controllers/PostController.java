56f87502d0914a31a3e39b95d18a3ba838bc72af
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-18 21:19:17 +0300

    PostController

---
+++ questapp/src/main/java/com/example/questapp/controllers/PostController.java
@@ -0,0 +1,48 @@
+package com.example.questapp.controllers;
+
+import com.example.questapp.entities.Post;
+import com.example.questapp.requests.PostCreateRequest;
+import com.example.questapp.requests.PostUpdateRequest;
+import com.example.questapp.services.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+
+        this.postService = postService;
+    }
+
+    @GetMapping
+    // /posts ya da /posts/{userId} gelen requeste göre id optional varsa o id yi yoksa tüm listeyi dönecek
+    public List<Post> getAllPosts(@RequestParam Optional<Long> userId){
+
+        return postService.getAllPosts(userId);
+    }
+
+    @PostMapping
+    public Post createOnePost(@RequestBody PostCreateRequest newPostRequest){
+        return postService.createOnePost(newPostRequest);
+    }
+    @GetMapping("/{postId}")
+    public Post getOnePost(@PathVariable Long postId){
+        return postService.getOnePostById(postId);
+    }
+
+    @PutMapping("/{postId}")
+    public Post updateOnePost(@PathVariable Long postId, @RequestBody PostUpdateRequest updatePost){
+        return postService.updateOnePostById(postId,updatePost);
+    }
+
+    @DeleteMapping("/{postId}")
+    public void deleteOnePost(@PathVariable Long postId){
+        postService.deleteOnePostById(postId);
+    }
+}
