37eae3fdf5c1b73edf904dba3a770f66ad2722ab
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-09-18 21:58:43 +0300

    LikeRepository

--- questapp/src/main/java/com/example/questapp/repos/LikeRepository.java
+++ questapp/src/main/java/com/example/questapp/repos/LikeRepository.java
@@ -2,6 +2,16 @@ package com.example.questapp.repos;
 
 import com.example.questapp.entities.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    List<Like> findByUserIdAndPostId(Long userId, Long postId);
+
+    List<Like> findByUserId(Long userId);
+
+    List<Like> findByPostId(Long postId);
+
 }
